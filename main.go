e762671bf4958facec94a100aeb9f02ffe984889
Colton Palmer <colton@coltonpalmer.com>
2024-02-05 19:42:00 -0500

    added path validation

--- main.go
+++ main.go
@@ -3,13 +3,27 @@ package main
 import (
   "fmt"
   "flag"
+  "os"
 )
 
 func main() {
-  markdownDir := flag.String("markdown-dir", ".",
-  "Path to directory containing Markdown files to be converted into HTML")
+  var markdownPath string
+  flag.StringVar(&markdownPath, "markdown-path", ".",
+`Path to file or directory containing Markdown to be converted into HTML.
+Defaults to current directory.`)
 
   flag.Parse()
 
-  fmt.Println("markdown-dir:", *markdownDir)
+  fileInfo, err := os.Stat(markdownPath);
+  if  err != nil {
+    fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+    os.Exit(1)
+  }
+
+  if !fileInfo.IsDir() {
+    fmt.Println("Path is a file", markdownPath)
+    return
+  }
+
+  fmt.Println("Path is a directory", markdownPath)
 }
